97ef94902b4c323c81ace7e33d7456157077cfc0
ElrohirGT <elrohirgt@gmail.com>
2025-01-22 11:48:26 -0600

    feat: Added load_buffer function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import re
 import csv
 
+TAMAÑO_BUFFER = 10
+
+
+# Código base para iniciar
+# def cargar_buffer(entrada, inicio, tamano_buffer):
+#     buffer = entrada[inicio : inicio + tamano_buffer]
+#     if len(buffer) < tamano_buffer:
+#         buffer.append("eof")
+#
+#     for i in range(10 - len(buffer)):
+#         buffer.append("")
+#     return buffer
+
+
 with open("page.html", "r", encoding="utf-8") as htmlFile:
     content = htmlFile.read()
 
